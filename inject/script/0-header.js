75058d4665476d1f5858804537952c022b028751
make-github-pseudonymous-again <make-github-pseudonymous-again@users.noreply.github.com>
2021-08-02 13:44:32 +0000

    Deploying to gh-pages from @ infoderm/healthone@816c14929727333308bba1a756f5879f2ac7eac8 🚀

--- inject/script/0-header.js
+++ inject/script/0-header.js
@@ -17,7 +17,7 @@ domReady(() => {
 	header.insertBefore(projectname, header.firstChild);
 
 	const testlink = document.querySelector('header > a[data-ice="testLink"]');
-	testlink.href = 'https://coveralls.io/github/infoderm/healthone';
+	testlink.href = 'https://codecov.io/gh/infoderm/healthone';
 	testlink.target = '_BLANK';
 
 	const searchBox = document.querySelector('.search-box');
