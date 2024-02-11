b7dcfd991906b9f93634e96838fe7aae1cd0efd4
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2024-02-11 17:33:04 +0100

    :arrow_up: deps: Upgrade @faker-js/faker to v8.4.1.

--- src/anonymize.js
+++ src/anonymize.js
@@ -7,7 +7,7 @@ const SEX_ALLOWED = ['male', 'female'];
 function fakenn(birthdate, sex) {
 	const yymmdd = Number.parseInt(dateformat(birthdate, 'yyMMdd'), 10);
 	const xxx =
-		faker.datatype.number({min: 0, max: 499}) * 2 + (sex === 'male' ? 1 : 0);
+		faker.number.int({min: 0, max: 499}) * 2 + (sex === 'male' ? 1 : 0);
 	const yymmddxxx = yymmdd * 1000 + xxx;
 	const _cd = yymmddxxx % 97;
 	const cd = _cd === 0 ? 97 : _cd;
@@ -17,29 +17,29 @@ function fakenn(birthdate, sex) {
 }
 
 export default function anonymize(document) {
-	document.identifier = faker.company.companyName();
+	document.identifier = faker.company.name();
 	document.reference = String(
-		faker.datatype.number({min: 10_000_000, max: 99_999_999}),
+		faker.number.int({min: 10_000_000, max: 99_999_999}),
 	);
-	document.requestor = faker.name.lastName();
+	document.requestor = faker.person.lastName();
 
-	document.patient.lastname = faker.name.lastName();
-	document.patient.firstname = faker.name.firstName();
+	document.patient.lastname = faker.person.lastName();
+	document.patient.firstname = faker.person.firstName();
 	document.patient.sex = faker.helpers.arrayElement(SEX_ALLOWED);
-	document.patient.birthdate = faker.date.past(AGE_MAX);
-	document.patient.municipality = faker.address.city();
-	document.patient.streetandnumber = faker.address.streetAddress();
-	document.patient.zip = faker.address.zipCode();
+	document.patient.birthdate = faker.date.past({years: AGE_MAX});
+	document.patient.municipality = faker.location.city();
+	document.patient.streetandnumber = faker.location.streetAddress();
+	document.patient.zip = faker.location.zipCode();
 	document.patient.nn = fakenn(
 		document.patient.birthdate,
 		document.patient.sex,
 	);
 
 	if (document.mutuality) {
-		document.mutuality.id = String(faker.datatype.number({min: 0, max: 999}));
+		document.mutuality.id = String(faker.number.int({min: 0, max: 999}));
 		document.mutuality.coverage = '';
 		document.mutuality.holder = '';
-		document.mutuality.kg1 = String(faker.datatype.number({min: 0, max: 999}));
-		document.mutuality.kg2 = String(faker.datatype.number({min: 0, max: 999}));
+		document.mutuality.kg1 = String(faker.number.int({min: 0, max: 999}));
+		document.mutuality.kg2 = String(faker.number.int({min: 0, max: 999}));
 	}
 }
