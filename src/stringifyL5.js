e6dd6e0c1adab0d10ee6c530d91800443455f2e9
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2025-01-27 18:51:56 +0100

    :boom: fix!: Handle multiple specialties in reports.
    BREAKING CHANGE: Previous JSON schema did not account for that and we
    implemented this change without implementing backwards-compatibility.

--- src/stringifyL5.js
+++ src/stringifyL5.js
@@ -1,11 +1,13 @@
 export default function* stringifyL5(record, options) {
-	const {reference, speciality, text} = record;
+	const {reference, sections} = record;
 
-	for (const line of text) {
-		const parts = ['L5', reference, speciality, '', '', '', '', line];
+	for (const {speciality, text} of sections) {
+		for (const line of text) {
+			const parts = ['L5', reference, speciality, '', '', '', '', line];
 
-		if (options.trailing.L5) parts.push('');
+			if (options.trailing.L5) parts.push('');
 
-		yield parts;
+			yield parts;
+		}
 	}
 }
